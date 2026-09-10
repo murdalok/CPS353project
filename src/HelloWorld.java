99de1c68c016dac38d9d25a6002ab05a9f5bce87
Adam Hockenjos <lokbomb@gmail.com>
2026-09-10 14:04:16 -0400

    Create HelloWorld.java

---
+++ src/HelloWorld.java
@@ -0,0 +1,66 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World, lets see if I can break a rule about method length")
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
+        System.out.println("5");
+        System.out.println("6");
+        System.out.println("7");
+        System.out.println("8");
+        System.out.println("9");
+        System.out.println("10");
+        System.out.println("11");
+        System.out.println("12");
+        System.out.println("13");
+        System.out.println("14");
+        System.out.println("15");
+        System.out.println("16");
+        System.out.println("17");
+        System.out.println("18");
+        System.out.println("19");
+        System.out.println("20");
+        System.out.println("21");
+        System.out.println("22");
+        System.out.println("23");
+        System.out.println("24");
+        System.out.println("25");
+        System.out.println("26");
+        System.out.println("27");
+        System.out.println("28");
+        System.out.println("29");
+        System.out.println("30");
+        System.out.println("31");
+        System.out.println("32");
+        System.out.println("33");
+        System.out.println("34");
+        System.out.println("35");
+        System.out.println("36");
+        System.out.println("37");
+        System.out.println("38");
+        System.out.println("39");
+        System.out.println("40");
+        System.out.println("41");
+        System.out.println("42");
+        System.out.println("43");
+        System.out.println("44");
+        System.out.println("45");
+        System.out.println("46");
+        System.out.println("47");
+        System.out.println("48");
+        System.out.println("49");
+        System.out.println("50");
+        System.out.println("51");
+        System.out.println("52");
+        System.out.println("53");
+        System.out.println("54");
+        System.out.println("55");
+        System.out.println("56");
+        System.out.println("57");
+        System.out.println("58");
+        System.out.println("59");
+        System.out.println("60");
+    }
+}
